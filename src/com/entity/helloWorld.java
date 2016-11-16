76d8c7090afc4877976a0b11362f52193e6b9103
mcSunny <18039110459@163.com>
2016-11-16 18:14:27 +0800

    Initial commit

---
+++ src/com/entity/helloWorld.java
@@ -0,0 +1,10 @@
+package com.entity;
+
+/**
+ * Created by edz on 2016/11/16.
+ */
+public class helloWorld {
+    public static  void main (String[] args){
+        System.out.println("我的第一个程序");
+    }
+}
