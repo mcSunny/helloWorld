8d0bf21c3c3b3afde7f7473295a62875dfc6a21a
mcSunny <18039110459@163.com>
2016-12-01 17:18:04 +0800

    update system

--- src/com/entity/helloWorld.java
+++ src/com/entity/helloWorld.java
@@ -6,5 +6,7 @@ package com.entity;
 public class helloWorld {
     public static  void main (String[] args){
         System.out.println("我的第一个程序");
+        System.out.println("这是我改动的地方，commit");
+        System.out.println("这是我改动的地方，tijiaotijiao");
     }
 }
